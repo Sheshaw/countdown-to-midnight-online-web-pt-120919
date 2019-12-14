c0afbbcbe611d42a6f3b15aead1f60977f609359
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-14 06:42:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
- def countdown(x = 10)
-   while x > 0
-   puts "#{x} SECOND(S)!"
-   x -= 1
+ def countdown(integer)
+   while integer > 0
+   puts "#{integer} SECOND(S)!"
+   integer -= 1
   end
   print "HAPPY NEW YEAR!"
 end
 
-countdown(x = 10)
+countdown(integer)
