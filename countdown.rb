850939b7e2a88499ee342d3f70e398228a7b7083
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-14 06:45:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,10 @@ end
 
 countdown(integer)
 
-def countdown_with_sleep
-  
+def countdown_with_sleep(integer)
+   while integer > 0
+   puts "#{integer} SECOND(S)!"
+   integer -= 1
+  end
+  print "HAPPY NEW YEAR!"
 end
